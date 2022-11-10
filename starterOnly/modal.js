9f84be659d31d9ed98155304e0276f991cae0fc8
Nicolas <nicolas.mazaud.sio@gmail.com>
2022-11-10 21:15:10 +0100

    Formulaire fini

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,13 +8,15 @@ function editNav() {
 }
 
 // DOM Elements
+const form = document.forms["reserve"];
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn = document.querySelector(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
+let isValid = true;
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.addEventListener("click", launchModal);
 
 // launch modal form
 function launchModal() {
@@ -24,4 +26,94 @@ function launchModal() {
 // Close modal form
 const closeForm = modalClose.addEventListener("click", () => {
   modalbg.style.display = "none";
+});
+
+// Regex definitions
+const regexName = /([A-Za-z\-\s]*)/;
+const regexEmail = /^([\w\-\.]+)@((?:[\w]+\.)+)([a-zA-Z]+)/;
+const regexQty = /\d{1,2}/;
+
+// Error
+const showError = (element) => {
+  isValid = false;
+  element.closest(".formData").dataset.errorVisible = true;
+};
+const closeError = (element) => {
+  element.closest(".formData").dataset.errorVisible = false;
+};
+
+// Validation form
+const validate = form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  isValid = true;
+  
+  // Validation firstname
+  if (
+   s form.element["first"].value.trim() === "" ||
+    !regexName.test(form.elements["first"].value.trim())
+  ) {
+    showError(form.elements["first"]);
+  } else {
+    closeError(form.elements["first"]);
+  }
+
+  // Validation lastname
+  if (
+    form.elements["last"].value.trim() === "" ||
+    !regexName.test(form.elements["last"].value.trim())
+  ) {
+    showError(form.elements["last"]);
+  } else {
+    closeError(form.elements["last"]);
+  }
+
+  // Validation email
+  if (
+    form.elements["email"].value.trim() === "" ||
+    !regexEmail.test(form.elements["email"].value.trim())
+  ) {
+    showError(form.elements["email"]);
+  } else {
+    closeError(form.elements["email"]);
+  }
+
+  // Validation quantity
+  if (
+    form.elements["quantity"].value.trim() === "" ||
+    !regexQty.test(form.elements["quantity"].value.trim())
+  ) {
+    showError(form.elements["quantity"]);
+  } else {
+    closeError(form.elements["quantity"]);
+  }
+
+  // Validation cgu checked
+  if (!form.elements["cgu"].checked) {
+    showError(form.elements["cgu"]);
+  } else {
+    closeError(form.elements["cgu"]);
+  }
+
+  //Validation location
+  if (!document.querySelector('input[name="location"]:checked')) {
+    showError(document.querySelector('input[name="location"]'));
+  } else {
+    closeError(document.querySelector('input[name="location"]'));
+  }
+
+  //Validation birthdate
+  if (
+    form.elements["birthdate"].value === "" ||
+    parseInt(form.elements["birthdate"].value.split("-")[0]) <= 1945
+  ) {
+    showError(form.elements["birthdate"]);
+  } else {
+    closeError(form.elements["birthdate"]);
+  }
+
+  if (isValid) {
+    form.style.display = "none";
+    document.querySelector(".validation").style.display = "flex";
+  }
 });
